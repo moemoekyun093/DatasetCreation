cc5910b428bd59c352483597991fa0afe34c56ab
Aditya <moemoekyun093@gmail.com>
2026-04-08 00:40:17 +0530

    made reranker

--- reranker.py
+++ reranker.py
@@ -18,7 +18,7 @@ warnings.filterwarnings("ignore")
 LOG_PATH = "table_scores.txt"
 OUTPUT_PATH = "table_retrieval_dataset.json"
 
-NUM_EXAMPLES = 100
+NUM_EXAMPLES = 1000
 TIMEOUT = 5
 MIN_TABLES = 3
 
